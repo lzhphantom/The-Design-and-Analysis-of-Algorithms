b5f520854c0920e6034a37465b2ee63be64dc88f
ratiki <987090402@qq.com>
2019-04-17 10:42:23 +0900

    update Euclid

--- ch1/Euclid/euclid.go
+++ ch1/Euclid/euclid.go
@@ -1,10 +1,34 @@
 package Euclid
 
-func Euclid(m, n int) int {
+import "fmt"
+
+func Euclid(m, n int) (int, error) {
+	if m == 0 || n == 0 {
+		return 0, fmt.Errorf("%d doesn't have the greatest common divisor with any natural number", 0)
+	}
 	for n != 0 {
 		r := m % n
 		m = n
 		n = r
 	}
-	return m
+	return m, nil
+}
+
+func GcdSerialInt(m, n int) (int, error) {
+	if m == 0 || n == 0 {
+		return 0, fmt.Errorf("%d doesn't have the greatest common divisor with any natural number", 0)
+	}
+	var t int
+	if m > n {
+		t = n
+	} else {
+		t = m
+	}
+	for t > 0 {
+		if m%t == 0 && n%t == 0 {
+			break
+		}
+		t--
+	}
+	return t, nil
 }
--- ch1/Euclid/euclid_test.go
+++ ch1/Euclid/euclid_test.go
@@ -25,3 +25,30 @@ func TestEuclid(t *testing.T) {
 		}
 	}
 }
+
+func TestGcdSerialInt(t *testing.T) {
+	var tests = []struct {
+		m    int
+		n    int
+		want int
+	}{
+		{0, 0, 0},
+		{60, 24, 12},
+		{33, 21, 3},
+		{1238, 125, 1},
+		{123456789, 456789, 3},
+		{987654321, 123456789, 9},
+		{13, 11, 10},
+	}
+
+	for _, test := range tests {
+		got, err := GcdSerialInt(test.m, test.n)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != test.want {
+			t.Errorf("GcdSerialInt(%d,%d) => %d, want %d",
+				test.m, test.n, got, test.want)
+		}
+	}
+}
